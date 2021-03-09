5071952b5589c340bc36970dc3848abf340cb33d
3xlerman <lermanworks@gmail.com>
2021-03-09 16:45:38 +0300

    task 1296 finished

--- algorithmsAndDataStructures/task1296.go
+++ algorithmsAndDataStructures/task1296.go
@@ -1,1 +1,31 @@
 package main
+
+import "fmt"
+
+func main() {
+
+	var N int
+	var numbers []int
+
+	fmt.Scan(&N)
+
+	for i := 0; i < N; i++ {
+		numbers = append(numbers, 0)
+		fmt.Scan(&numbers[i])
+	}
+
+	var max int
+	var maxEnd int
+
+	for i := 0; i < len(numbers); i++ {
+		maxEnd += numbers[i]
+		if max < maxEnd {
+			max = maxEnd
+		}
+		if maxEnd < 0 {
+			maxEnd = 0
+		}
+	}
+	fmt.Println(max)
+
+}
